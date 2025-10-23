89946a6ff717378c41cc3d0634608d1bac2712da
VISORKILLEN <viktor.gustavsson@live.se>
2025-10-23 12:11:59 +0200

    put in some comments

--- Labb 1 - Introduktion till OOP/Circle.cs
+++ Labb 1 - Introduktion till OOP/Circle.cs
@@ -6,11 +6,14 @@
 
         public float _pi = 3.141f;
 
+        //A contructor is run when creating a new circle
+        //The 'radie' is assaigned to the Radie property.
         public Circle(int radie)
         {
             Radie = radie;
         }
 
+        //Method that calculate the area of the circle
         public double GetArea()
         {
             Console.WriteLine($"Radien är på en cirkel är {Radie} cm.");
@@ -18,12 +21,13 @@
             return Radie * Radie * _pi;
         }
 
+        //Method that calculate the circumfernece of the circle
         public double GetCircumference()
         {
             Console.WriteLine($"Omkretsen på den cirklen är {2 * Radie * _pi}");
             return 2 * Radie * _pi;
         }
-
+        //Method that calculate the volume of a sphear if u just have the radius.
         public double GetVolume()
         {
             Console.WriteLine($"Om man gör cirklen till ett klot så blir volymen {Radie * Radie * Radie * _pi * 4/3} cm^3\n");
--- Labb 1 - Introduktion till OOP/Program.cs
+++ Labb 1 - Introduktion till OOP/Program.cs
@@ -4,9 +4,13 @@
     {
         static void Main(string[] args)
         {
+
+            //i created 2 new radiuses.
             Circle radie1 = new Circle(5);
             Circle radie2 = new Circle(6);
 
+            //Here i call for the methods to the radiuses that  i created in the Circle.cs
+            //so it does the calucaltion in the console
             radie1.GetArea();
             radie1.GetCircumference();
             radie1.GetVolume();
